b2e964684e7f0052e8d7d5eeec44144880eb6d78
webdev4422 <67459982+webdev4422@users.noreply.github.com>
2023-07-02 13:42:58 +0000

    Set up a Template Engine

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', './views/pug');
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -12,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.route('/').get((req, res) => {
-
+  res.render('index.pug')
 });
 
 const PORT = process.env.PORT || 3000;
