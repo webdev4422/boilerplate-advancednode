5d2c81eea1895bfbf23a4af9c4df49be6384b12b
webdev4422 <67459982+webdev4422@users.noreply.github.com>
2023-07-02 14:02:01 +0000

    Set up Passport

--- server.js
+++ server.js
@@ -3,10 +3,21 @@ require('dotenv').config();
 const express = require('express');
 const myDB = require('./connection');
 const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 app.set('view engine', 'pug');
 app.set('views', './views/pug');
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
+passport.initialize()
+passport.session()
 
 fccTesting(app); //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'));
