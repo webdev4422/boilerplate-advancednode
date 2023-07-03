f22468f1a3a467c80c05f2d2d2ddd8d32aa02c33
webdev4422 <67459982+webdev4422@users.noreply.github.com>
2023-07-03 14:29:33 +0000

    Implementation of Social Authentication II

--- auth.js
+++ auth.js
@@ -1,9 +1,22 @@
+require('dotenv').config()
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const GitHubStrategy = require('passport-github').Strategy
 const bcrypt = require('bcrypt')
 const { ObjectID } = require('mongodb')
 
-module.exports = function (app, myDataBase) {
+module.exports = function(app, myDataBase) {
+  passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: 'https://boilerplate-advancednode.webdev4422.repl.co/auth/github/callback'
+  },
+    function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+      //Database logic here with callback containing your user object
+    }
+  ));
+
   passport.use(
     new LocalStrategy((username, password, done) => {
       myDataBase.findOne({ username: username }, (err, user) => {
