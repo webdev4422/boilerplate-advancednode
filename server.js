e4ca6a538facd3613be55a672f4b13c0417d29ba
user <user@gmail.com>
2023-07-03 15:06:07 +0300

    How to Use Passport Strategies

--- server.js
+++ server.js
@@ -28,21 +28,24 @@ app.use('/public', express.static(process.cwd() + '/public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.route('/').get((req, res) => {
-  res.render('index', { title: 'Connected to Database', message: 'Please log in' })
-})
-
 myDB(async (client) => {
   const myDataBase = await client.db('database').collection('users')
 
-  passport.serializeUser((user, done) => {
-    done(null, user._id)
+  app.route('/').get((req, res) => {
+    console.log('Connected to Database')
+    res.render('index', {
+      title: 'Connected to Database',
+      message: 'Please log in',
+      showLogin: true,
+    })
   })
 
-  passport.deserializeUser((id, done) => {
-    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-      done(null, doc)
-    })
+  app.route('/login').post(passport.authenticate('local', { failureRedirect: '/' }), (req, res) => {
+    res.redirect('/profile')
+  })
+
+  app.route('/profile').get((req, res) => {
+    res.render('profile')
   })
 
   passport.use(
@@ -56,6 +59,16 @@ myDB(async (client) => {
       })
     })
   )
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id)
+  })
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc)
+    })
+  })
 }).catch((e) => {
   app.route('/').get((req, res) => {
     res.render('index', { title: e, message: 'Unable to connect to database' })
