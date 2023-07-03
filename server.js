f50b723431cb245954bc57ef30c6e1fb50568f7c
user <user@gmail.com>
2023-07-03 13:45:27 +0300

    Serialization of a User Object

--- server.js
+++ server.js
@@ -1,34 +1,47 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
-const session = require('express-session');
-const passport = require('passport');
+'use strict'
+require('dotenv').config()
+const express = require('express')
+const myDB = require('./connection')
+const fccTesting = require('./freeCodeCamp/fcctesting.js')
+const session = require('express-session')
+const passport = require('passport')
+const { ObjectID } = require('mongodb')
 
-const app = express();
-app.set('view engine', 'pug');
-app.set('views', './views/pug');
+const app = express()
+app.set('view engine', 'pug')
+app.set('views', './views/pug')
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: true,
-  saveUninitialized: true,
-  cookie: { secure: false }
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+)
 passport.initialize()
 passport.session()
 
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+passport.serializeUser((user, done) => {
+  done(null, user._id)
+})
+
+passport.deserializeUser((id, done) => {
+  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+  done(null, null)
+  // })
+})
+
+fccTesting(app) //For FCC testing purposes
+app.use('/public', express.static(process.cwd() + '/public'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' });
-});
+  res.render('index', { title: 'Hello', message: 'Please log in' })
+})
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
-});
+  console.log('Listening on port ' + PORT)
+})
