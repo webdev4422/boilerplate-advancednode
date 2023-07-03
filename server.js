4a868dc368c1dc08851624afae9e6b4cd2e98bdd
user <user@gmail.com>
2023-07-03 13:58:14 +0300

    Implement the Serialization of a Passport User

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 'use strict'
+const PORT = process.env.PORT || 3000
 require('dotenv').config()
 const express = require('express')
 const myDB = require('./connection')
@@ -22,26 +23,33 @@ app.use(
 passport.initialize()
 passport.session()
 
-passport.serializeUser((user, done) => {
-  done(null, user._id)
-})
-
-passport.deserializeUser((id, done) => {
-  // myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
-  done(null, null)
-  // })
-})
-
 fccTesting(app) //For FCC testing purposes
 app.use('/public', express.static(process.cwd() + '/public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.route('/').get((req, res) => {
-  res.render('index', { title: 'Hello', message: 'Please log in' })
+  res.render('index', { title: 'Connected to Database', message: 'Please log in' })
 })
 
-const PORT = process.env.PORT || 3000
+myDB(async (client) => {
+  const myDataBase = await client.db('database').collection('users')
+
+  passport.serializeUser((user, done) => {
+    done(null, user._id)
+  })
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(null, doc)
+    })
+  })
+}).catch((e) => {
+  app.route('/').get((req, res) => {
+    res.render('index', { title: e, message: 'Unable to connect to database' })
+  })
+})
+
 app.listen(PORT, () => {
   console.log('Listening on port ' + PORT)
 })
